ff1f20a5bc4a877fd52d40c3c23bd6660154be81
Aline GAUNET <a.gaunet@ecole-ipssi.net>
2026-03-25 12:32:34 +0100

    Step 3

--- main.py
+++ main.py
@@ -16,12 +16,20 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        sorted_students = sorted(self.students, key=lambda s: s.matter1, reverse=True)
+        print("\nClassement matière 1 :")
+        for s in sorted_students:
+            print(s.name, s.matter1)
+
 if __name__ == "__main__":
     school_class = SchoolClass()
 
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+
+    school_class.rank_matter_1()
 
     print("Étudiants ajoutés :")
     for s in school_class.students:
